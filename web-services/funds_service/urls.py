bb3ea3890801471164969716f2e7d3c3635c8d54
Mark Henwood <mcbhenwood@icloud.com>
2021-03-25 07:44:01 +0000

    Correct import ordering

--- web-services/funds_service/urls.py
+++ web-services/funds_service/urls.py
@@ -1,5 +1,6 @@
-from funds_service.views import FundViewSet
 from rest_framework.routers import DefaultRouter
+
+from funds_service.views import FundViewSet
 
 router = DefaultRouter()
 router.register(r"funds", FundViewSet, basename="fund")
